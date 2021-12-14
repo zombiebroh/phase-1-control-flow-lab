b7a8565b06566b2af77a51060caedda3ded11f89
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-14 08:53:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result = "No can do."
+  if (distance <= 400) {
+    result = "This one is on me!"
+  } else if (distance > 2000 && distance <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500) { 
+    result
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return 'Thank you.'
+    default: 
+    return "Bye."
+  }
 }
